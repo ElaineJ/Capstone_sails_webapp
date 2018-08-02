9ed32c878ce0ff28907491e90ba74cfb64a74b9c
JulianJason <jason.julian27@gmail.com>
2018-08-03 07:30:35 +0800

    DUEB

--- capstone-sails/api/controllers/patients/fetch.js
+++ capstone-sails/api/controllers/patients/fetch.js
@@ -38,6 +38,7 @@ module.exports = {
     const dateNormalizer = require('../../services/normalizeDate');
     const DOB = dateNormalizer.normalize(inputs.DOB);
 
+    sails.log.info(NRIC, "AND " + DOB + "AND" + inputs.DOB);
     const PATIENTS_GET = 'select * from patients WHERE nric = \'' + NRIC + '\' AND dob = \'' + inputs.DOB + '\'';
 
     const queryResults = await sails.sendNativeQuery(PATIENTS_GET);
